0b6db6e516b4f60ba28734af2a6e09773490b3c8
adityarev <aditya.vritalia@gmail.com>
2018-05-28 05:52:46 +0700

    Update categories/subcategory/show

--- resources/views/categories/subcategory/show.blade.php
+++ resources/views/categories/subcategory/show.blade.php
@@ -1,59 +1,83 @@
 @extends('layouts.master')
 @section('content')
 <style>
-    .table-category-information > tbody > tr {
-        border-top: 1px solid rgb(221, 221, 221);
-    }
-
-    .table-category-information > tbody > tr:first-child {
-        border-top: 0;
-    }
-
-    .table-category-information > tbody > tr > td {
-        border-top: 0;
-    }
-
     .toppad {
         margin-top:20px;
-    }
-
-    .panel > .list-group {
-        margin-bottom: 0;
-    }
-
-    .panel > .list-group .list-group-item {
-        border-width: 1px 0;
-    }
-
-    .panel > .list-group .list-group-item:first-child {
-        border-top-right-radius: 0;
-        border-top-left-radius: 0;
-    }
-
-    .panel > .list-group .list-group-item:last-child {
-        border-bottom: 0;
-    }
-    .panel-heading + .list-group .list-group-item:first-child {
-        border-top-width: 0;
     }
 </style>
 
 <div class="container">
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xs-offset-0 col-sm-offset-0 col-md-offset-1 col-lg-offset-1 toppad" >
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title"><strong>Subcategory Name</strong></h3>
+                    <h3 class="panel-title"><strong>SubCategory Name</strong></h3>
                 </div>
-                <ul class="list-group">
-                    <a href="#" class="list-group-item">Item 1</a>
-                    <a href="#" class="list-group-item">Item 2</a>
-                    <a href="#" class="list-group-item">Item 3</a>
-                    <a href="#" class="list-group-item">Item 4</a>
-                    <a href="#" class="list-group-item">Item 5</a>
-                </ul>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 toppad" >
+                            <div class="panel panel-default">
+                                <a href="#">
+                                    <div class="panel-body">
+                                        <div class="col-md-12 col-lg-12" id="item" style="padding:0px; opacity: 0.4">
+                                            <img alt="300x200" style="width: 100%" src="http://placehold.it/200x150">
+                                        </div>
+                                        <div class="col-md-12 col-lg-12" style="padding:0px">
+                                            <h4>Sabun</h4>
+                                            <span class="fa fa-dollar" style="margin-right: 5px"></span>12
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 toppad" >
+                            <div class="panel panel-default">
+                                <a href="#">
+                                    <div class="panel-body">
+                                        <div class="col-md-12 col-lg-12" id="item" style="padding:0px; opacity: 0.4">
+                                            <img alt="300x200" style="width: 100%" src="http://placehold.it/200x150">
+                                        </div>
+                                        <div class="col-md-12 col-lg-12" style="padding:0px">
+                                            <h4>Sabun</h4>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 toppad" >
+                            <div class="panel panel-default">
+                                <a href="#">
+                                    <div class="panel-body">
+                                        <div class="col-md-12 col-lg-12" id="item" style="padding:0px; opacity: 0.4">
+                                            <img alt="300x200" style="width: 100%" src="http://placehold.it/200x150">
+                                        </div>
+                                        <div class="col-md-12 col-lg-12" style="padding:0px">
+                                            <h4>Sabun</h4>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function () {
+        $(".panel .panel-default").mouseenter(function () {
+            $('#item').animate({
+                opacity: "1.0"
+            }, 250);
+        }).mouseleave(function () {
+            $('#item').animate({
+                opacity: "0.4"
+            }, 250);
+        });
+    });
+</script>
 @stop
