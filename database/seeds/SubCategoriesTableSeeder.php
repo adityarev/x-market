810ab9699c9d29ebcd219205a10751196d484b3e
arisetyaji <arisetyaji0@gmail.com>
2018-05-29 20:55:47 +0700

    data

--- database/seeds/SubCategoriesTableSeeder.php
+++ database/seeds/SubCategoriesTableSeeder.php
@@ -54,7 +54,77 @@ class SubCategoriesTableSeeder extends Seeder
 
         SubCategory::create([            
             'category_id' => 4,
-            'sub_category_name' => 'Film',
+            'sub_category_name' => 'Film dan Musik',
         ]);        
+
+        SubCategory::create([
+            'category_id' => 5,
+            'sub_category_name' => 'Mainan Anak'
+        ])
+
+        SubCategory::create([
+            'category_id' => 5,
+            'sub_category_name' => 'Figure'
+        ])
+
+        SubCategory::create([
+            'category_id' => 6,
+            'sub_category_name' => 'Dapur'
+        ])
+
+        SubCategory::create([
+            'category_id' => 6,
+            'sub_category_name' => 'Interior Rumah'
+        ])
+        
+        SubCategory::create([
+            'category_id' => 6,
+            'sub_category_name' => 'Eksterior Rumah'
+        ])
+        
+        SubCategory::create([
+            'category_id' => 7,
+            'sub_category_name' => 'souvenir acara'
+        ])
+        
+        SubCategory::create([
+            'category_id' => 7,
+            'sub_category_name' => 'Kado'
+        ])
+        
+        SubCategory::create([
+            'category_id' => 8,
+            'sub_category_name' => 'Mobil'
+        ])
+        
+        SubCategory::create([
+            'category_id' => 8,
+            'sub_category_name' => 'Motor'
+        ])
+
+        SubCategory::create([
+            'category_id' => 8,
+            'sub_category_name' => 'Sepeda'
+        ])
+
+        SubCategory::create([
+            'category_id' => 9,
+            'sub_category_name' => 'Perlengkapan Olahraga'
+        ])
+
+        SubCategory::create([
+            'category_id' => 9, 
+            'sub_category_name' => 'Baju Olahraga'
+        ])
+
+        SubCategory::create([
+            'category_id' => 10,
+            'sub_category_name' => 'Game'
+        ])
+
+        SubCategory::create([
+            'category_id' => 10
+            'sub_category_name' => 'Aplikasi'
+        ])
     }
 }
