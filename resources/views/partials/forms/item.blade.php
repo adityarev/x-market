7806180baf2a55ff4211a2d72c5d1779863aea1f
adityarev <aditya.vritalia@gmail.com>
2018-05-29 07:34:56 +0700

    Fixing item's partial form

--- resources/views/partials/forms/item.blade.php
+++ resources/views/partials/forms/item.blade.php
@@ -69,7 +69,7 @@
                     <img alt="Item Pic" src="http://pngimg.com/uploads/book/book_PNG2114.png" class="img-responsive" style="height: 100%">
                 </div>
                 <div class="col-md-4 col-lg-4" style="height: 200px">
-                    <button class="btn btn-danger" style="margin: 83px auto">
+                    <button class="btn btn-danger" id="delete-btn" style="margin: 83px auto">
                         <span class="glyphicon glyphicon-trash" style="margin-right: 5px"></span>
                         Delete
                     </button>
@@ -77,22 +77,46 @@
             </div>
         </div>
     </div>
-    
-    <div class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1">
-        <div class="panel panel-primary">
-            <div class="panel-body">
-                <div class="col-md-8 col-lg-8" style="height: 200px">
-                    <img alt="Item Pic" src="http://pngimg.com/uploads/book/book_PNG2114.png" class="img-responsive" style="height: 100%">
+
+    <!-- Delete Modal -->
+    @foreach($item->images as $image)
+        <div class="modal fade" id="delete-modal-{{ $image->id }}" role="dialog">
+            <div class="modal-dialog">
+            {!! Form::model($image, ['url' => '/items/'.$item->user->username.'/'.$item->item_name, 'method' => 'delete']) !!}
+            <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header" style="padding:35px 50px;">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4><span class="glyphicon glyphicon-warning-sign"></span> Warning!</h4>
+                    </div>
+                    <div class="modal-body" style="padding:40px 50px;">
+                        <h5>This photo would be delete from your preview. Are you sure ?</h5>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="{{ url('items/'.$item->user->username.'/'.$item->item_name.'/delete') }}" data-method="delete">
+                            <button type="submit" class="btn btn-danger btn-default">
+                                <span class="glyphicon glyphicon-trash"></span>Yes
+                            </button>
+                        </a>
+                        <button type="submit" class="btn btn-success" data-dismiss="modal">
+                            <span class="glyphicon glyphicon-remove"></span> No
+                        </button>
+                    </div>
                 </div>
-                <div class="col-md-4 col-lg-4" style="height: 200px">
-                    <button class="btn btn-danger" style="margin: 83px auto">
-                        <span class="glyphicon glyphicon-trash" style="margin-right: 5px"></span>
-                        Delete
-                    </button>
-                </div>
+                {!! Form::close() !!}
             </div>
         </div>
-    </div>
+    @endforeach
+
+    @foreach($item->images as $image)
+        <script>
+            $(document).ready( function () {
+                $('#delete-btn-{{ $image->id }}').click( function () {
+                    $('#delete-modal-{{ $image->id }}').modal();
+                });
+            });
+        </script>
+    @endforeach
 @endif
 
 <div class="form-group">
