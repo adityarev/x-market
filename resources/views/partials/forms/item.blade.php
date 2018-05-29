091f2570133459aca20ae471b28a133bbee87a83
adityarev <aditya.vritalia@gmail.com>
2018-05-29 07:19:27 +0700

    Update items/edit view

--- resources/views/partials/forms/item.blade.php
+++ resources/views/partials/forms/item.blade.php
@@ -43,11 +43,57 @@
 </div>
 
 <div class="form-group">
+    <label class="col-md-4 control-label" for="Upload photo">Upload photo</label>
+    <div class="col-md-6">
+        {!! Form::file('item_image') !!}
+    </div>
+</div>
+
+<div class="form-group">
     <label class="col-md-4 control-label" for="Description">Description</label>
     <div class="col-md-6">        
         {!! Form::textarea ('item_description', null, ['class' => 'form-control', 'placeholder' => 'Description']) !!}        
     </div>
 </div>
+
+@if (\Request::is('*/edit'))
+    <div class="form-group">
+        <label class="col-md-4 control-label" for="Preview Pictures">Preview Pictures</label>
+        <div class="col-md-6"></div>
+    </div>
+
+    <div class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1">
+        <div class="panel panel-primary">
+            <div class="panel-body">
+                <div class="col-md-8 col-lg-8" style="height: 200px">
+                    <img alt="Item Pic" src="http://pngimg.com/uploads/book/book_PNG2114.png" class="img-responsive" style="height: 100%">
+                </div>
+                <div class="col-md-4 col-lg-4" style="height: 200px">
+                    <button class="btn btn-danger" style="margin: 83px auto">
+                        <span class="glyphicon glyphicon-trash" style="margin-right: 5px"></span>
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1">
+        <div class="panel panel-primary">
+            <div class="panel-body">
+                <div class="col-md-8 col-lg-8" style="height: 200px">
+                    <img alt="Item Pic" src="http://pngimg.com/uploads/book/book_PNG2114.png" class="img-responsive" style="height: 100%">
+                </div>
+                <div class="col-md-4 col-lg-4" style="height: 200px">
+                    <button class="btn btn-danger" style="margin: 83px auto">
+                        <span class="glyphicon glyphicon-trash" style="margin-right: 5px"></span>
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
 
 <div class="form-group">
     <label class="col-md-4 control-label" ></label>
